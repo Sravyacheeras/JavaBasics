752e0cb27f5480b93ed4e8b71bd7d7f104e79af4
Sravyacheeras <111745568+Sravyacheeras@users.noreply.github.com>
2022-08-28 11:34:50 +0530

    New changes (#1)
    Co-authored-by: Sankar Cheerala <shankarchirala@gmail.com>

---
+++ src/sample/ForLoopNew.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class ForLoopNew {
+
+	public static void main(String[] args) {
+
+		int arr[] = {10,20,30,40};
+		for(Integer i : arr) {
+			System.out.println(i);
+		}
+	}
+
+}

7a0ade329ddab44fc62dd76598e4bc3ae9ff283e
Sankar Cheerala <shankarchirala@gmail.com>
2022-08-28 11:29:11 +0530

    New changes

--- src/sample/ForLoopDemo.java
+++ src/sample/ForLoopDemo.java
@@ -5,7 +5,7 @@ public class ForLoopDemo {
 	public static void main(String[] args) {
 
 		for(int i=0;i<100;i++) {
-			
+		//added some comment here	
 			if(i%2==0) {
 			System.out.println(i);
 			}
